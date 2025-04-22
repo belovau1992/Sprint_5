5560a75baec4a4b85721343122312b6b29396707
Andrew Below <belovau1992@yandex.com>
2025-04-22 19:07:57 +0300

    optimization the use of locators in test_constructor.py

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -2,37 +2,39 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
+from Sprint_5.locators import Locators
+
 
 class TestConstructor():
     def test_sauces_section(self,driver):
         sauces_tab = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, "//div[contains(text(), 'Соусы')]"))
+            EC.element_to_be_clickable(*Locators.SAUCES_SECTION)
         )
         sauces_tab.click()
         active_tab = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located(
-                (By.XPATH, "//div[contains(@class, 'tab_type_current') and contains(text(), 'Соусы')]"))
+                *Locators.SAUCES_SECTION)
         )
         assert active_tab.is_displayed()
 
     def test_bread_section(self,driver):
         bread_tab = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, "//div[contains(text(), 'Булки')]"))
+            EC.element_to_be_clickable(*Locators.BREAD_SECTION)
         )
         bread_tab.click()
         active_tab = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located(
-                (By.XPATH, "//div[contains(@class, 'tab_type_current') and contains(text(), 'Булки')]"))
+                *Locators.BREAD_SECTION)
         )
         assert active_tab.is_displayed()
 
     def test_fillings_section(self,driver):
         fillings_tab = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, "//div[contains(text(), 'Начинки')]"))
+            EC.element_to_be_clickable(*Locators.FILLINGS_SECTION)
         )
         fillings_tab.click()
         active_tab = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located(
-                (By.XPATH, "//div[contains(@class, 'tab_type_current') and contains(text(), 'Начинки')]"))
+                *Locators.FILLINGS_SECTION)
         )
         assert active_tab.is_displayed()
